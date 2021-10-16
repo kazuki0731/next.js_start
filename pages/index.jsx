8b551a94d7595b38525225cc3ba3175399e8b5d7
kawabata <kazuki.kawabata02@gmail.com>
2021-10-17 02:26:44 +0900

    change

--- pages/index.jsx
+++ pages/index.jsx
@@ -21,7 +21,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>gitで変更</p>
+        <p>エディタで編集</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
